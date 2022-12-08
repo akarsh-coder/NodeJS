c4b152d673f857d41e203d0630a836e96c862b4d
akarsh-coder <kra.getit@gmail.com>
2022-12-08 12:54:00 +0530

    added styles to pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../utils/path");
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
-router.post("/product", (req, res, next) => {
+// router.get("/add-product", (req, res, next) => {
+//   res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+// });
+router.post("/add-product", (req, res, next) => {
   //   res.send(`<h1>${req.body}</h1>`); didn't work
   console.log(req.body);
   res.redirect("/");
